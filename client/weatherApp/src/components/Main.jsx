27c94f0d8098cb8bed664a36d6fd7ae5076e4ebd
Linkoln195 <gilioko195@gmail.com>
2024-09-07 15:47:00 +0200

    city title + mobile version

--- client/weatherApp/src/components/Main.jsx
+++ client/weatherApp/src/components/Main.jsx
@@ -1,19 +1,24 @@
 import classes from "./Main.module.css"
 
 import temperatureIcon from "../assets/Images/temperatureIcon.png"
+import locationIcon from "../assets/Images/locationIcon.png"
 import { useEffect, useState } from "react";
 
 
 
 const Main = () => {
     const [temperature, setTemperature] = useState("Loading...");
+    const [city, setCity] = useState("")
 
     useEffect(() => {
         const getData = async () => {
             const url = "https://api.openweathermap.org/data/2.5/weather?lat=51.1854&lon=6.44172&appid=a4674d9ec6f6aa3b00fd1df3067505a0&units=metric";
             const fullResponse = await fetch(url)
-            const response = await fullResponse.json();
-            setTemperature(response.main.temp);
+            await fullResponse.json().then((response) => {
+                setTemperature(response.main.temp)
+                setCity(response.name)
+                console.log(response)
+            })
         }
 
         getData();
@@ -22,8 +27,15 @@ const Main = () => {
     return (
         <div className={classes.mainBox}>
             <div className={classes.weatherBox}>
-                <img src={temperatureIcon} className={classes.temperatureIcon}></img>
-                <h2 className={classes.temperatureTitle}>{Math.round(temperature)}</h2>
+                <div className={classes.cityBox}>
+                    <h2 className={classes.cityTitle}>{city}</h2>
+                    <img src={locationIcon} className={classes.locationIcon}></img>
+                </div>
+
+                <div className={classes.temperatureBox}>
+                    <img src={temperatureIcon} className={classes.temperatureIcon}></img>
+                    <h2 className={classes.temperatureTitle}>{Math.round(temperature)}</h2>
+                </div>
             </div>
         </div>
     )
